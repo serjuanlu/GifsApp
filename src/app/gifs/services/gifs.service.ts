a013ff5f929e5cb0ac627a1ef7f5a54f4a80238b
Juan Luis Serrano Vilchez <serjuanluvil@gmail.com>
2025-01-19 15:23:26 +0100

    validacion en servicio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -5,41 +5,36 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
   // Creamos el atributo que va a guardar el historial de etiquetas
-  private _historialEtiquetas:string[]=[];
+  private _historialEtiquetas: string[] = [];
 
   // El getter del historial
-  get historialEtiquetas(){
+  get historialEtiquetas() {
     return [...this._historialEtiquetas];
   }
 
+
+
   // Para agregar una nueva etiqueta al array
-  buscarEtiqueta(etiqueta:string):void{
+  buscarEtiqueta(etiqueta: string): void {
     // Vaciamos la cadena de espacios
-    etiqueta=etiqueta.trim();
+    etiqueta = etiqueta.trim();
     // Si la longitud es 0, no se va a guardar
-    if(etiqueta.length===0){
-      window.alert('La cadena esta vacia');
+    if (etiqueta.length === 0) {
+      console.warn('La etiqueta esta vacia, no se va a agregar');
       return;
     }
-    // Llamamos al metodo de validar la etiqueta, que va a devolver una etiqueta
-    etiqueta=this.validarEtiqueta(etiqueta);
+    // Comprobamos si es minuscula,
+    this._historialEtiquetas = this._historialEtiquetas.filter(
+      (etiquetaGuardada) =>
+        etiquetaGuardada.toLowerCase() !== etiqueta.toLowerCase()
+    );
+    // Metemos la etiqueta al principio
     this._historialEtiquetas.unshift(etiqueta);
+    // Y si hubiese 10 o mas, las sacamos
+    if (this._historialEtiquetas.length > 10) {
+      this._historialEtiquetas.pop();
+    }
   }
 
-  // Metodo que hace las validaciones de las etiquetas
-  validarEtiqueta(etiqueta:string):string{
-       // Lo pasamos a minuscula
-       etiqueta=etiqueta.toLowerCase();
-       // El metodo unshift va a insertar la etiqueta al principio del array
-       if(this._historialEtiquetas.includes(etiqueta)){
-        window.alert(`${etiqueta} ya esta en la lista`);
-         // Igualamos el array de etiquetas al array filtrado sin la etiqueta repetida
-         this._historialEtiquetas=this.historialEtiquetas.filter((etiquetas)=>etiquetas!=etiqueta);
-       }
-       if(this._historialEtiquetas.length>=10){
-         this.historialEtiquetas.pop();
-       }
-       return etiqueta;
-  }
   constructor() { }
 }
